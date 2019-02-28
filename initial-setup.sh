95518467c0a3b8a8af82f190057228c9a724e3b9
Swanand Pagnis <swanand.pagnis@gmail.com>
2019-02-28 11:44:00 +0530

    Remove unnecessary pip upgrade and psycopg2-binary

--- initial-setup.sh
+++ initial-setup.sh
@@ -2,7 +2,6 @@
 
 apt-get update
 apt-get install -y build-essential libreadline6 libreadline6-dev postgresql postgresql-contrib python-pip libpq-dev python-dev
-pip install --upgrade pip
-pip install psycopg2-binary pgcli
+pip install pgcli
 
 source ${SHARED_DIR}/scripts/pg-setup.sh
